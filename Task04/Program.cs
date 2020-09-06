32270b1e3f28d66670af9421b2463159af0f3037
Исломбек Туракулов <iuturakulov@edu.hse.ru>
2020-09-06 22:01:40 +0500

    Task04

---
+++ Task04/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Threading;
+
+namespace Task04
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Программа получает от пользователя значения напряжения U и сопротивления R, вычислить силу тока  I = U/R и потребляемую мощность  P = U2/R электрической цепи.");
+            
+            Console.Write("Введите значение напряжения ( U ): ");
+            double U = double.Parse(Console.ReadLine());
+
+            Console.Write("Введите значение сопротивления ( R ): ");
+            double R = double.Parse(Console.ReadLine());
+
+
+            Console.WriteLine("Вычисляем силу тока формулой I = U/R");
+            Thread.Sleep(2000);
+
+            Console.WriteLine("Ответ: " + (U / R).ToString("F4") + " А ");
+
+            Console.WriteLine("Вычисляем потребляемую мощность  P = U2/R электрической цепи");
+            Thread.Sleep(1000);
+
+            Console.WriteLine("Ответ: " + (Math.Pow(U, 2) / R).ToString("F4"));
+
+            Console.WriteLine("Нажмите на ENTER , чтобы выйти..");
+            Console.ReadKey();
+        }
+    }
+}
