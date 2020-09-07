78ccd1aae1766745d5163da24f6fc42dffc504aa
Исломбек Туракулов <iuturakulov@edu.hse.ru>
2020-09-07 15:25:17 +0500

    Lesson_ASCIIDECODER

---
+++ Task03/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Task03
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n;
+            Console.WriteLine("Введите значения от 32 до 127: ");
+
+            while (!int.TryParse(Console.ReadLine(), out n) || (n < 32) || (n >= 127))
+            {
+                Console.WriteLine("Неверные данные");
+            }
+            Console.WriteLine((char)(n));
+        }
+    }
+}
