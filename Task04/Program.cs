a5e5e103a4b82bb9437db58d358a1103bd4e63e2
Исломбек Туракулов <iuturakulov@edu.hse.ru>
2020-09-08 17:04:34 +0500

    Task04 Физика

--- Task04/Program.cs
+++ Task04/Program.cs
@@ -7,6 +7,8 @@ namespace Task04
     {
         static void Main(string[] args)
         {
+            // Ознакомление информации про консольное приложение
+
             Console.WriteLine("Программа получает от пользователя значения напряжения U и сопротивления R, вычислить силу тока  I = U/R и потребляемую мощность  P = U2/R электрической цепи.");
             
             Console.Write("Введите значение напряжения ( U ): ");
@@ -18,12 +20,14 @@ namespace Task04
 
             Console.WriteLine("Вычисляем силу тока формулой I = U/R");
             Thread.Sleep(2000);
-
+            
+            // Вычисление силы тока
             Console.WriteLine("Ответ: " + (U / R).ToString("F4") + " А ");
 
             Console.WriteLine("Вычисляем потребляемую мощность  P = U2/R электрической цепи");
             Thread.Sleep(1000);
 
+            // Вычисление потребляемой мощности
             Console.WriteLine("Ответ: " + (Math.Pow(U, 2) / R).ToString("F4"));
 
             Console.WriteLine("Нажмите на ENTER , чтобы выйти..");
