bbe28f74f496473b0d41a9e0b182c490931de633
Исломбек Туракулов <iuturakulov@edu.hse.ru>
2020-09-10 18:14:17 +0500

    Task 3 / 10.09.2020

--- Seminar _ 2/CW2/Program.cs
+++ Seminar _ 2/CW2/Program.cs
@@ -6,7 +6,7 @@ namespace CW2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("");
         }
     }
 }
---
+++ Seminar _ 2/Task_3/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace Task_3
+{
+    class Program
+    {
+        public static void SortValue(ref int x, ref int y, ref int z)
+        {
+            // Вспомогательные переменные
+            int a1, a2 , a3;
+
+            a1 = (x <= y) ? (z < x ? z : x) : (y < z ? y : z);
+            a3 = (x >= y) ? (z > x ? z : x) : (y > z ? y : z);
+            a2 = (z >= x) ? (x > y ? x : y) : (x < y ? x : y);
+
+            int min = a1;
+            int mid = a2;
+            int max = a3;
+
+            Console.WriteLine($"Минимальное значение: {min} ");
+            Console.WriteLine($"Среднее значение {mid} ");
+            Console.WriteLine($"Максимальное значение {max} ");
+
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                int x, y, z;
+                do
+                {
+                    Console.Write("Введите x = " );
+                    x = int.Parse(Console.ReadLine());
+                    Console.Write("Введите y = ");
+                    y = int.Parse(Console.ReadLine());
+                    Console.Write("Введите z = ");
+                    z = int.Parse(Console.ReadLine());
+
+                    // Через метод вывожу ответ
+
+                    SortValue(ref x, ref y, ref z);
+
+                    Console.WriteLine("Для выхода нажмите на ESC");
+                }
+                while (Console.ReadKey().Key != ConsoleKey.Escape);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
