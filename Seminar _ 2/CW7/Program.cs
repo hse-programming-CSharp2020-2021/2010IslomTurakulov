ef481881695016cc3b33ea7d0122608f43c1852d
Исломбек Туракулов <iuturakulov@edu.hse.ru>
2020-09-11 12:16:25 +0500

    Task_04 11.09.2020

---
+++ Seminar _ 2/CW7/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Runtime.CompilerServices;
+/*
+* Написать программу с использованием двух методов. Первый метод возвращает дробную и целую часть числа. 
+* Второй метод возвращает квадрат и корень из числа. В основной программе пользователь вводит дробное число. 
+* Программа должна вычислить, если это возможно, значение корня, квадрата числа, выделить целую и дробную часть из числа.
+*/
+
+namespace CW7
+{
+    class Program
+    {
+
+        static void DoubleSplit(double doubleNumber)
+        {
+            int fractLength = doubleNumber.ToString().Split(',')[1].Length;
+            Console.WriteLine();
+            (int)Math.Round(doubleNumber)
+        }
+        public int Fract
+        {
+            get { return (int)((doubleNumber - Int) * Math.Pow(10, fractLength)); }
+        }
+        public double Doub
+        {
+            get { return doubleNumber; }
+        }
+        static void Main(string[] args)
+        {
+            try
+            {
+                do
+                {
+                    Console.WriteLine("Введите дробное число: ");
+                    double doubleNumber = double.Parse(Console.ReadLine());
+                    DoubleSplit ds1 = new DoubleSplit(doubleNumber);
+
+                } while (Console.ReadKey().Key != ConsoleKey.Escape);
+                
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Ошибка {ex.Message}");
+            }
+        }
+    }
+}
---
+++ Seminar _ 2/Task_04/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Linq;
+
+namespace Task_4
+{
+    /*
+       Метод получает параметр типа uint, в дальнейших операциях преобразует его в тип char, 
+       затем делает проверку на принадлежность данного аргумента к коду цифры или буквы кириллицы 
+       и выводит необходимые сообщения по условию задачи.
+     */
+    class Program
+    {
+        /// <summary>
+        /// Метод получает uint и затем делает проверку на принадлежность данного аргумента к коду цифры или буквы кириллицы
+        /// </summary>
+        /// <param name="numValue"> Параметр типа uint</param>
+        /// <returns></returns>
+        public static string myMethod(uint numValue)
+        {
+            string str;
+
+            return str = numValue >= (uint)'А' && numValue <= (uint)'я' ? "Это буква!" :
+                         numValue >= (uint)'0' && numValue <= (uint)'9' ? "Это цифра!" :
+                         "Это не буква и не цифра!";
+        }
+
+        static void Main(string[] args)
+        {
+            try
+            {
+                do
+                {
+                    Console.Clear();
+
+                    uint numValue;
+                    Console.Write("Введите ASCII код: ");
+                    while (!uint.TryParse(Console.ReadLine(), out numValue))
+                    {
+                        Console.WriteLine("Произошла ошибка!");
+                        Console.WriteLine("Попробуйте снова");
+                    }
+
+                    Console.WriteLine($"{myMethod(numValue)}");
+
+                    Console.WriteLine("Нажмите ESC для выхода");
+                }
+                while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Причина ошибки {ex.Message}");
+            }
+        }
+    }
+}
