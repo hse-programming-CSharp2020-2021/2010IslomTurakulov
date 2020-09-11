875d8549aa8ad38293d39bae3098c61df49bd6fe
Исломбек Туракулов <iuturakulov@edu.hse.ru>
2020-09-11 11:49:54 +0500

    CW 11.09.2020

--- Seminar _ 2/CW2/Program.cs
+++ Seminar _ 2/CW2/Program.cs
@@ -1,18 +1,47 @@
 ﻿using System;
-
+using System.Security.Cryptography;
+/*
+* Ввести натуральное трехзначное число Р. 
+* Найти наибольшее целое число, которое можно получить, переставляя цифры числа Р.
+*/
 namespace CW2
 {
     class Program
     {
+        /// <summary>
+        ///  метод возвращает наибольшее и наименьшее число
+        /// </summary>
+        /// <param name="c">Трёхзначное число</param>
+        public static void SortValue(ref int c)
+        {
+            // Вспомогательные переменные
+            int x = c / 100;
+            int y = (c - (x * 100))/10;
+            int z = c - (x * 100) - (y * 10);
+
+            int min = x > y ? (y > z ? z : y) : (x > z ? z : x);
+            int max = x > y ? (x > z ? x : y) : (y > z ? y : z);
+            int middle = y > z ? (z > x ? z : (x > y ? y : x)) : (y > x ? y : (z > x ? x : z));
+
+            c = max * 100 + middle * 10 + min; 
+            Console.WriteLine($"Минимальное значение: {min} ");
+            Console.WriteLine($"Среднее значение {middle} ");
+            Console.WriteLine($"Максимальное значение {max} ");
+
+        }
         static void Main(string[] args)
         {
             try
             {
-                int numValue;
-                Console.WriteLine("Введите трёхзначное число: ");
-                numValue = int.Parse(Console.ReadLine());
-                
+                do
+                {
+                    Console.WriteLine("Введите трёхзначное число: ");
+                    int numValue = int.Parse(Console.ReadLine());
+                    
+                    SortValue(ref numValue);
+                } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
             }
+                
             catch (Exception ex)
             {
                 Console.WriteLine(ex.Message);
