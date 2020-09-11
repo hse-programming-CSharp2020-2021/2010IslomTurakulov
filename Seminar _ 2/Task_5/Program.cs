a722da23fbe355421f1c48a9b0d9e4febfd38eae
Исломбек Туракулов <iuturakulov@edu.hse.ru>
2020-09-11 12:06:19 +0500

    Task_5 11.09.2020

---
+++ Seminar _ 2/Task_5/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Task_5
+{
+    /*
+      Выделение отдельных цифр натурального числа
+      Задача:
+      Ввести трехзначное натуральное число и напечатать его цифры "столбиком".
+    */
+
+    class Program
+    {
+        static void Splitter(int numberValue)
+        {
+            char[] arr =  numberValue.ToString().ToCharArray();
+            foreach (var value in arr)
+                Console.WriteLine(value);
+        }
+        static void Main(string[] args)
+        {
+            // Обработка Исключений
+            try
+            {
+                
+                int numberValue;
+                // Можно использовать if else Для проверки
+                do
+                {
+                    Console.WriteLine("Введите трёхзначное число: ");
+                } while (!int.TryParse(Console.ReadLine(), out numberValue) | numberValue < 100 | numberValue > 999);
+                // Через метод передаю значение
+                Splitter(numberValue);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Причина ошибки {ex.Message}");
+            }
+
+
+        }
+    }
+}
