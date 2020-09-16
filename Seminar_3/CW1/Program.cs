fa6b141016027ca27599590f83d9a69b30ae6972
Исломбек Туракулов <iuturakulov@edu.hse.ru>
2020-09-16 21:24:32 +0500

    CW1 16.09.2020

---
+++ Seminar_3/CW1/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace CW1
+{
+    /*
+     * Написать метод, находящий трехзначное десятичное число s, 
+     * все цифры которого одинаковы и которое представляет собой сумму первых членов натурального ряда
+     */
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            do
+            {
+                int a = 1;
+
+                while (a * (a + 1) % 111 != 0)
+                {
+                    a++;
+                    int d = (a * (a + 1)) / 2;
+                    Console.WriteLine(d);
+                }
+                Console.WriteLine("Нажмите на ESC чтобы выйти");
+
+            } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
+
+        }
+    }
+}
