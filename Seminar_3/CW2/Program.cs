abbe97e81fd5f7b841720b0eb9e3164f615d5713
Исломбек Туракулов <iuturakulov@edu.hse.ru>
2020-09-16 21:50:30 +0500

    CW2 16.09.2020

---
+++ Seminar_3/CW2/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Linq;
+
+namespace CW2
+{
+    /* Написать метод, преобразующий число переданное в качестве параметра в число, 
+     * записанное теми же цифрами, но идущими в обратном порядке. 
+     */
+
+    class Program
+    {
+        /// <summary>
+        /// Выводит число в обратном порядке
+        /// </summary>
+        /// <param name="a">Число</param>
+        static void ReversedNumber(int a)
+        {
+            Console.WriteLine(a.ToString().Reverse().ToArray());
+        }
+        static void Main(string[] args)
+        {
+            int numValue;
+            do
+            {
+                Console.Write("Напишите любое число: ");
+                string inputValue = Console.ReadLine();
+
+                // Проверка на данные (string,char,float)
+                if (!int.TryParse(inputValue, out numValue))
+                {
+                    Console.WriteLine("Неккоректные данные!");
+                }
+                else
+                {
+                    Console.Write("Обратное число: ");
+                    ReversedNumber(numValue);
+                }
+
+                Console.WriteLine("Нажмите на ESC чтобы выйти..");
+            } while(Console.ReadKey(true).Key != ConsoleKey.Escape);
+        }
+    }
+}
