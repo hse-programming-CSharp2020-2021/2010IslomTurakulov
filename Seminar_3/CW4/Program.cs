8a2e7f83186272cc68cc256b68835a763c9c639b
Исломбек Туракулов <iuturakulov@edu.hse.ru>
2020-09-16 23:16:59 +0500

    CW4 16.09.2020

--- Seminar_3/CW4/Program.cs
+++ Seminar_3/CW4/Program.cs
@@ -6,14 +6,6 @@ namespace CW4
 {
     class Program
     {
-        static void Main(string[] args)
-        {
-            double x, y, g;
-            Console.WriteLine("Введите x");
-            g = G(x, y);
-            Console.WriteLine(g.ToString());
-            Console.ReadKey();
-        }
         public static double G(double x, double y)
         {
             if (x < y && x > 0)
@@ -28,6 +20,33 @@ namespace CW4
             {
                 return 0.5 * x * y;
             }
+        }
+        static void ReadNumber(out double num)
+        {
+            while (!double.TryParse(Console.ReadLine(), out num))
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Введены некорректные данные!");
+                Console.WriteLine("Пожалуйста повторите попытку");
+                Console.ForegroundColor = ConsoleColor.Gray;
+                Console.WriteLine();
+                Console.Write("Введите значение: ");
+            }
+        }
+        static void Main(string[] args)
+        {
+            double x, y, g;
+            do
+            {
+                Console.Write("Введите значение: ");
+                ReadNumber(out x);
+                Console.Write("Введите значение: ");
+                ReadNumber(out y);
+
+                g = G(x, y);
+                Console.WriteLine(g.ToString("F4"));
+                Console.WriteLine("Нажмите ESC , чтобы выйти");
+            } while (Console.ReadKey(true).Key != ConsoleKey.Escape);
         }
     }
 }
