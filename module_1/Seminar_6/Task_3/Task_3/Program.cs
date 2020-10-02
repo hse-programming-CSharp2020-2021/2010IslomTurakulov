6f82d3a1df5f7042dafba7e34356e40fef203783
Исломбек Туракулов <iuturakulov@edu.hse.ru>
2020-10-02 23:12:10 +0500

    Module_1

--- module_1/Seminar_6/Task_3/Task_3/Program.cs
+++ module_1/Seminar_6/Task_3/Task_3/Program.cs
@@ -12,32 +12,40 @@ namespace Task_3
 
         static double SinFinder(double x)
         {
+            // Создаём новый List.
             List<double> sinRow = new List<double>();
-            const double eps = 1e-15;  // погрешность вычислений
+            // погрешность вычислений.
+            const double eps = 1e-15; 
             //double sum = 0;
             //double result = x;
             sinRow.Add(x);
             int digits = 1;
             while (Math.Abs(sinRow[sinRow.Count - 1]) > eps)
             {
-                // добавляет к числу +2 
+                // добавляет к числу +2.
                 digits = digits + 2;
                 sinRow.Add((-sinRow[sinRow.Count - 1]) * x * x / (digits * (digits - 1)));
             }
+            //Вывод результата.
             Console.WriteLine(sinRow.Count);
+            // Вывод суммы.
             return sinRow.Sum();
         }
         static void Main()
         {
+            //Создаём переменную x.
             double x;
             do
             {
                 Console.WriteLine("Enter the digit x: ");
             } while (!double.TryParse(Console.ReadLine(), out x));
-
+            // Проверяем метод и ставим переменные.
             double result = SinFinder(x);
+            //Результат вывода.
             Console.WriteLine($"Результат вывода: {result}");
             Console.WriteLine("Сравнение исходного и изменённого: ");
+
+            // Сравнивает результаты.
             if (x.Equals(result))
                 Console.Write("Верно");
             else
