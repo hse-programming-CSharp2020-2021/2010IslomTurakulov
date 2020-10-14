55b6d10ce760d2909dfda1b5850f717dac11097a
Исломбек Туракулов <iuturakulov@edu.hse.ru>
2020-10-14 16:32:14 +0500

    module_1

---
+++ module_1/Seminar_9/Lesson/Lesson/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+
+namespace Lesson
+{
+    class Program
+    {
+        static void DirectoryOverview(string path, int level)
+        {
+            var directories = Directory.GetDirectories(path);
+
+            for (int i = 0; i < directories.Length; i++)
+
+            {
+                var directory = directories[i];
+                var directoryInfo = new DirectoryInfo(directory);
+                Console.WriteLine($"{directory}\n attributes: {directoryInfo.Attributes} Creation time: {directoryInfo.CreationTime} Last update: {directoryInfo.LastWriteTime}");
+                DirectoryOverview(directory, level - 1);
+            }
+        }
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.WriteLine("Введите уровень: ");
+                int level = int.Parse(Console.ReadLine());
+                DirectoryOverview(@"../../../", level);
+               
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+    }
+}
